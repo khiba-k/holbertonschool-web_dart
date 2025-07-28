a6d3c9ac95cba1b337d59249e2004270ad589d3f
khiba-k <khibakoenane@gmail.com>
2025-07-28 15:47:46 +0200

    task: CML App determines if arg is postive/negative/zero

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -2,7 +2,6 @@ void main(List<String> args) {
   /*
   Write Your code below 
    */
-  assert(args.isNotEmpty, 'Please provide a number as a command-line argument');
   int number = int.parse(args[0]);
   assert(number >= 80, 'The score must be bigger or equal to 80');
   print('You Passed');
