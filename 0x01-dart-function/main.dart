537d3dacad683fdb060b49635c8d48bb236326fe
khiba-k <khibakoenane@gmail.com>
2025-07-31 18:55:25 +0200

    dev: task5, function calculates area

--- 0x01-dart-function/main.dart
+++ 0x01-dart-function/main.dart
@@ -1,7 +1,7 @@
-import '3-factors.dart';
+import '5-calculate-area.dart';
 
-void main() {
-  print(fact(0));
-  print(fact(1));
-  print(fact(5));
+main() {
+  print(calculateArea(20, 7));
+  print(calculateArea(10, 7));
+  print(calculateArea(8.5, 3.7));
 }
